9865a6379a8bfcc2ef87e3f61e3928ae3eeb24b7
Philippe Cuaz <11057785+philippe-cuaz@users.noreply.github.com>
2026-04-16 15:45:07 +0200

    "commit"

--- channels.go
+++ channels.go
@@ -1,10 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	//variable :=  make(chan type)  '<-' operator
 	greeting := make(chan string)
 	greetString := "Hello"
 
-	greeting
+	go func() {
+		greeting <- greetString // blocking because itis continuously trying to receive values,
+		// it is ready to receive continuous flow of data.
+	}()
+
+	receiver := <-greeting
+
+	fmt.Println(receiver)
+
 }
