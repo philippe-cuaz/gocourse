56e204d6cdd8ead363b8c05fdc1dbe9cafefba9b
Philippe Cuaz <11057785+philippe-cuaz@users.noreply.github.com>
2026-04-23 15:57:11 +0200

    "Commit"

--- wait_groups.go
+++ wait_groups.go
@@ -1,12 +1,28 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-func worker(id int,wg *sync.WaitGroup){
-		
-	}
-func main()
-{
-	
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Worker %d starting\n", id)
+	time.Sleep(time.Second) // simulate some time spent on processing the task
+	fmt.Printf("Worker %d finished\n", id)
+}
 
+func main() {
+	var wg sync.WaitGroup
+	numWorkers := 3
+
+	wg.Add(numWorkers)
+
+	// Launch workers
+	for i := range numWorkers {
+		go worker(i, &wg)
+
+	wg.Wait()
+	fmt.Println("All workers finished")
 }
