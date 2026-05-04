17035c45785bab8c5029c3b5c98bfef95aa2225c
Philippe Cuaz <11057785+philippe-cuaz@users.noreply.github.com>
2026-05-04 16:36:39 +0200

    "Commit"

--- rest_api/simple_api/go.mod
+++ rest_api/simple_api/go.mod
@@ -1,3 +1,8 @@
 module simpleapi
 
 go 1.26.1
+
+require (
+	golang.org/x/net v0.53.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
+)
--- rest_api/simple_api/server.go
+++ rest_api/simple_api/server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 func main() {
@@ -16,7 +20,35 @@ func main() {
 	})
 	port := 3000
 
+	// Load the TLS cert and key
+	cert := "cert.pem"
+	key := "key.pem"
+
+	// Configure TLS
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	// Create a custom server
+	server := &http.Server{
+		Addr:      fmt.Sprintf(":%d", port),
+		Handler:   nil,
+		TLSConfig: tlsConfig,
+	}
+
+	// Enable http2
+	http2.ConfigureServer(server, &http2.Server{})
+
 	fmt.Println("Server is running on port:", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	err := server.ListenAndServeTLS(cert, key)
+	if err != nil {
+		log.Fatalln("Could not start server", err)
+	}
+	// HTTP 1.1 Server without TLS
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// if err != nil {
+	// 	log.Fatalln("Could not start server", err)
+	// }
 
 }
